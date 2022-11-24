4d488168d710acf83b49a3f545e7cab9b40bdc81
naeemahmedmb <naeemahmedmb@gmail.com>
2022-11-24 15:47:10 +0600

    looping coded adeed

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-let a = 10;
-let b = 20;
+//var firstNumber = 100;
+//var secondNumber = 200;
 
-document.write("Aritmetic:" +( a+b));
+//document.write("Aritmetic:" +( a+b));
 //document.write"a++:" +(a ++);
-b=a++;
-document.write("<br> a: " + a);
-document.write("<br> b: " +b);
-b= B+1;
-b+=1;
+//b=a++;
+//document.write("<br> a: " + a);
+//document.write("<br> b: " +b);
+//b= B+1;
+//b+=1;
+
+var digit = (Number(prompt("Enter any Number")));
+if(digit <= 100)
+    document.write("<= 100");
+else if digit(digit < 100 && digit <= 200)
+    document.write("> 100 & <=200");
+    else if(digit < 200 && digit <= 300)
+    document.write("> 200 & <=300");
+    else
+        document.write("Invalid");
+
+        //switch(digit){
+            //case :
+            //case :
+        //}
