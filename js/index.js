2e54f30678c64b4857927765d9fbc1463240e673
naeemahmedmb <naeemahmedmb@gmail.com>
2022-11-24 12:50:38 +0600

    javascript class mehrin anaynna mam

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+document.write("Aritmetic:" +( a+b));
+//document.write"a++:" +(a ++);
+b=a++;
+document.write("<br> a: " + a);
+document.write("<br> b: " +b);
+b= B+1;
+b+=1;
