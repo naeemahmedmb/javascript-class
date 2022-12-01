e051ea38577d8f79f4022f7621139e3d3ef4d656
naeemahmedmb <naeemahmedmb@gmail.com>
2022-12-01 12:38:27 +0600

    looping added

--- js/index.js
+++ js/index.js
@@ -9,17 +9,26 @@
 //b= B+1;
 //b+=1;
 
-var digit = (Number(prompt("Enter any Number")));
-if(digit <= 100)
-    document.write("<= 100");
-else if digit(digit < 100 && digit <= 200)
-    document.write("> 100 & <=200");
-    else if(digit < 200 && digit <= 300)
-    document.write("> 200 & <=300");
-    else
-        document.write("Invalid");
+// var digit = (Number(prompt("Enter any Number")));
+// if(digit <= 100)
+//     document.write("<= 100");
+// else if digit(digit<100 && digit <= 200)
+//     document.write("> 100 & <=200");
+//     else if(digit < 200 && digit <= 300)
+//     document.write("> 200 & <=300");
+//     else
+//         document.write("Invalid");
 
-        //switch(digit){
-            //case :
+//         //switch(digit){
+//             //case :
             //case :
         //}
+        for (var i= 1; i<= 10; i++){
+            document.write(i + "<br>");
+        }
+
+        var i = 1;
+        while(i<11){
+            document.write(i + "<br>");
+            i++;
+        }
